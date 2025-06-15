e257d993e9b51eb8469b1a748ead0c6bb0b80518
Veys69 <denissakhnevych2010@gmail.com>
2025-06-16 01:16:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 
+window.onload = function() {
+  if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark');
+  }
+};
+
 const toggle = document.getElementById('themeToggle');
 toggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
+  document.body.classList.toggle('dark');
+  if (document.body.classList.contains('dark')) {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'light');
+  }
 });
